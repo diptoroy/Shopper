1f66d4f16a34deb7c2db51583c38129ddb8ca717
diptoroy <diptoroy54@gmail.com>
2020-07-15 00:56:57 +0600

    wishList

--- app/src/main/java/com/atcampus/shopper/Adapter/WishlistAdapter.java
+++ app/src/main/java/com/atcampus/shopper/Adapter/WishlistAdapter.java
@@ -6,6 +6,7 @@ import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -28,12 +29,23 @@ public class WishlistAdapter extends RecyclerView.Adapter<WishlistAdapter.ViewHo
     @NonNull
     @Override
     public WishlistAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.wishlist_item_row,parent,false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.wishlist_item_row, parent, false);
         return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull WishlistAdapter.ViewHolder holder, int position) {
+
+        int resource = wishlistModels.get(position).getProductImage();
+        String title = wishlistModels.get(position).getProductName();
+        int freeCueponNo = wishlistModels.get(position).getProductCuepon();
+        String rating = wishlistModels.get(position).getProductRating();
+        int totalRating = wishlistModels.get(position).getProductTotalRating();
+        String price = wishlistModels.get(position).getProductPrice();
+        String cuttedPrice = wishlistModels.get(position).getProductCuttedPrice();
+        String deliSystem = wishlistModels.get(position).getProductDeliverySystem();
+
+        holder.setData(resource,title,freeCueponNo,rating,totalRating,price,cuttedPrice,deliSystem);
 
     }
 
@@ -54,7 +66,7 @@ public class WishlistAdapter extends RecyclerView.Adapter<WishlistAdapter.ViewHo
         private View productDivider;
         private TextView productCuttedPrice;
         private TextView productDeliverySystem;
-        private ImageButton deleteBtn;
+        private ImageView deleteBtn;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -72,8 +84,33 @@ public class WishlistAdapter extends RecyclerView.Adapter<WishlistAdapter.ViewHo
             deleteBtn = itemView.findViewById(R.id.delete_product);
         }
 
-        private void setData(){
-            
+        private void setData(int resource, String title, int freeCueponNo,String rating,int totalRating,String price,String cuttedPrice,String deliSystem) {
+            productImage.setImageResource(resource);
+            productName.setText(title);
+            if (freeCueponNo != 0) {
+                productCueponIcon.setVisibility(View.VISIBLE);
+                if (freeCueponNo == 1) {
+                    productCuepon.setText("Free " + freeCueponNo + " coupen");
+                } else {
+                    productCuepon.setText("Free " + freeCueponNo + " coupens");
+                }
+            } else {
+                productCueponIcon.setVisibility(View.INVISIBLE);
+                productCuepon.setVisibility(View.INVISIBLE);
+            }
+            productRating.setText(rating);
+            productTotalRating.setText(totalRating +"(rating)");
+            productPrice.setText(price);
+            productCuttedPrice.setText(cuttedPrice);
+            productDeliverySystem.setText(deliSystem);
+
+            deleteBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(itemView.getContext(),"Delete Item",Toast.LENGTH_LONG).show();
+                }
+            });
+
         }
     }
 }
--- app/src/main/java/com/atcampus/shopper/Fragment/WishlistFragment.java
+++ app/src/main/java/com/atcampus/shopper/Fragment/WishlistFragment.java
@@ -3,12 +3,19 @@ package com.atcampus.shopper.Fragment;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.atcampus.shopper.Adapter.WishlistAdapter;
+import com.atcampus.shopper.Model.WishlistModel;
 import com.atcampus.shopper.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,12 +26,31 @@ public class WishlistFragment extends Fragment {
         // Required empty public constructor
     }
 
+    private RecyclerView wishListRecyclerView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_wishlist, container, false);
+
+        wishListRecyclerView = view.findViewById(R.id.wishlist_recyclerView);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        linearLayoutManager.setOrientation(RecyclerView.VERTICAL);
+        wishListRecyclerView.setLayoutManager(linearLayoutManager);
+
+        List<WishlistModel> wishlistModelsList = new ArrayList<>();
+        wishlistModelsList.add(new WishlistModel(R.drawable.phone,"Iphone 12 Max",1,"4.5",46,"$799","$849","Cash On Delivery Available."));
+        wishlistModelsList.add(new WishlistModel(R.drawable.phone,"Iphone 11",2,"4.2",34,"$699","$749","Cash On Delivery Available."));
+        wishlistModelsList.add(new WishlistModel(R.drawable.phone,"Iphone 12 Max",0,"3.2",56,"$599","$649","No Cash On Delivery Available."));
+        wishlistModelsList.add(new WishlistModel(R.drawable.phone,"Iphone 8",0,"5.0",49,"$799","$849","Cash On Delivery Available."));
+        wishlistModelsList.add(new WishlistModel(R.drawable.phone,"Iphone 10",3,"4.0",16,"$799","$849","No Cash On Delivery Available."));
+        wishlistModelsList.add(new WishlistModel(R.drawable.phone,"Iphone 12 ",0,"5.0",93,"$799","$849","Cash On Delivery Available."));
+
+        WishlistAdapter wishlistAdapter = new WishlistAdapter(wishlistModelsList);
+        wishListRecyclerView.setAdapter(wishlistAdapter);
+        wishlistAdapter.notifyDataSetChanged();
         return view;
     }
 }
